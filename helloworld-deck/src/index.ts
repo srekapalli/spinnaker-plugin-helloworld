5b1f374af81888734b4dbe605005366ddd6040a6
Chris Thielen <cthielen@netflix.com>
2020-02-05 15:13:44 -0800

    refactor: use latest deck plugin api

--- helloworld-deck/src/index.ts
+++ helloworld-deck/src/index.ts
@@ -1,11 +1,5 @@
 import { customStage  } from './CustomStage'
 
-const plugin = {
-  name: 'spinnaker-sample-plugin',
-  initialize: function(pluginApi: any) {
-    console.log('init');
-    pluginApi.registerStage(customStage);
-  }
+export const plugin = {
+  stages: [customStage]
 };
-
-export { plugin };
